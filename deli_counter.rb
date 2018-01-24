a6a55bc7794b6e6fdc289d41c493641f74640694
Stephen Thurman <thurman024@gmail.com>
2018-01-24 21:51:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.size > 0 
+  if katz_deli.size > 0
     print "The line is currently "
     katz_deli.each_with_index do |person, index|
       print "#{index+1}. #{person}"
     end
   else
     print "The line is currently empty"
+  end
 end
 
 def take_a_number
